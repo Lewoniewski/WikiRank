3d1326109d1241a19222050e1a9f080a69dd339d
Lewoniewski <wlodzimierz.lewoniewski@gmail.com>
2019-02-11 21:51:38 +0100

    WikiRank API
    Parser for Python 3

---
+++ API/get_data_python3.py
@@ -0,0 +1,30 @@
+import requests, json
+
+url = 'https://api.wikirank.net/api.php'
+data = {"name": "Gniezno", "lang":"pl"}
+r = requests.post(url, json=data)
+js = json.loads(r.text)
+
+quality={}
+popularity={}
+
+for key,value in js["result"].items():
+    quality[key]=value["quality"]
+    popularity[key]=value["popularity"]
+
+limit=5
+
+print ("Wersje językowe o najwyższej jakości:")
+iteracja=0
+for nazwa in sorted(quality, key=quality.get, reverse=True):
+    iteracja+=1
+    if iteracja>limit: break
+    print (nazwa +" : "+str(quality[nazwa]))
+
+print ("-------------")
+print ("Najpopularniejsze wersje językowe:")
+iteracja=0
+for nazwa in sorted(popularity, key=popularity.get, reverse=True):
+    iteracja+=1
+    if iteracja>limit: break
+    print (nazwa +" : "+str(popularity[nazwa]))
